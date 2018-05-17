004aff71cf7d4e11d13bb265e7cff4cd53d9675d
DavidBednar <davidbednarba@gmail.com>
2018-05-17 16:46:59 +0200

    Removed reg2 and reset2 screens

--- src/app/main/content/pages/pages.module.ts
+++ src/app/main/content/pages/pages.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 
 import { LoginModule } from './authentication/login/login.module';
 import { RegisterModule } from './authentication/register/register.module';
-import { Register2Module } from './authentication/register-2/register-2.module';
 import { ForgotPasswordModule } from './authentication/forgot-password/forgot-password.module';
 import { ResetPasswordModule } from './authentication/reset-password/reset-password.module';
-import { ResetPassword2Module } from './authentication/reset-password-2/reset-password-2.module';
 import { LockModule } from './authentication/lock/lock.module';
 import { MailConfirmModule } from './authentication/mail-confirm/mail-confirm.module';
 import { MaintenanceModule } from './maintenance/maintenence.module';
@@ -17,10 +15,8 @@ import { SearchModule } from './search/search.module';
         // Auth
         LoginModule,
         RegisterModule,
-        Register2Module,
         ForgotPasswordModule,
         ResetPasswordModule,
-        ResetPassword2Module,
         LockModule,
         MailConfirmModule,
 
